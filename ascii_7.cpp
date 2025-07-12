247c229cbb0365fd4308ced0d1bb46823b01af31
joshi567 <gjoshitha567@gmail.com>
2025-07-12 09:12:00 +0530

    Add files via upload

---
+++ ascii_7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char x;
+    cout<<"character is " ;
+    cin>> x;
+    cout<< "ASCII  value of x is :" <<(int)x<<"\n";
+
+}
+
+
+
+
+   
+
+
+
+
+
+
+
