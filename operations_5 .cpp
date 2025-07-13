a642e9f49691ed06fea0e5e8ead4a828593828b7
joshi567 <gjoshitha567@gmail.com>
2025-07-13 09:07:13 +0530

    Add files via upload

---
+++ operations_5 .cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a=2,b=8;
+
+cout<<"the value of a+b:"<<a+b<<"\n";
+cout<<"the value of a-b:"<<a-b<<"\n";
+cout<<"the value of a*b:"<<a*b<<"\n";
+cout<<"the value of a/b:"<<a/b<<"\n";
+cout<<"the value of a%b:"<<a%b<<"\n";
+cout<<"the value of a++:"<<a++<<"\n";
+cout<<"the value of a--:"<<a--<<"\n";
+cout<<"the value of ++a:"<<++a<<"\n";
+cout<<"the value of --a:"<<a+b<<"\n";
+cout<<"the value of b++:"<<b++<<"\n";
+cout<<"the value of b--:"<<b--<<"\n";
+cout<<"the value of ++b:"<<++b<<"\n";
+cout<<"the value of --b:"<<--b<<"\n";
+cout<<"endl";
+    
+    return 0;
+}
