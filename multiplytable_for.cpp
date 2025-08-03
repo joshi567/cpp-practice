ced1a7ab37ac7d68474870c2593209add71aabe7
joshi567 <gjoshitha567@gmail.com>
2025-08-03 10:45:40 +0530

    Add files via upload

---
+++ multiplytable_for.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"the value of a: ";
+    cin>>a;
+    cout<<"Multiplication table of a: \n";
+    for (int i = 0; i <11; i++){
+        cout<<(a*i)<<"\n";
+    }
+    
+        
+    }
+    
+
