be798929c631b0dbaf739263036b4446b3671697
joshi567 <gjoshitha567@gmail.com>
2025-10-02 10:23:06 +0530

    Add files via upload

---
+++ math_operations.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float a,b ;
+    printf("enter a,b:\n");
+    scanf("%f""%f",&a,&b);
+    printf("a value:%.2f,b value:%.2f/n",a,b);
+
+    printf("square root of a:%.3f \n",sqrt(a));
+    printf("a raised to the power of b:%.2f \n",pow(a,b));
+    printf("absolute value of a:%.1f and b:%.1f \n",fabs(a),fabs(b));
+}
+
+
+
+
