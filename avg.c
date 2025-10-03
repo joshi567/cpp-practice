3410798770be7d83ce77224c526134b13dffc276
joshi567 <gjoshitha567@gmail.com>
2025-10-03 09:56:58 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d""%d""%d",&a,&b,&c);
+    printf("the value of a:%d,b:%d,c:%d\n",a,b,c);
+     int  avg=(a+b+c)/3;
+    printf("the average of the three numbers is:%d\n",avg);
+}
